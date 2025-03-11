60c9e581be683e683941bded5a13abbf8a7a8ced
Allifer <alliferlopes568@outlook.com>
2025-03-11 16:28:03 -0300

    Finalizado

---
+++ paginas/conexoes/crudServicos.php
@@ -0,0 +1,67 @@
+<?php
+    include_once('conexao.php');
+
+    $servico = "";
+    $data_servico = "";
+    $prestador = "";
+    $valor = "";
+    $descricao ="";
+    $mensagem = "";
+
+    // Buscar todos os prestadores de serviços cadastrados para exibição na tabela
+    try {
+        $sql = $conn->query('
+            SELECT 
+                s.id_servico,
+                s.servico,
+                s.data_servico,
+                s.prestador,
+                s.valor,
+                s.descricao
+            FROM 
+                tab_servicos s
+        ');
+        $servicos = $sql->fetchAll();
+    } catch (PDOException $erro) {
+        $mensagem = $erro->getMessage();
+    }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Adicionar um novo prestador de serviços
+    if (isset($_POST['btoAdicionar'])) {
+        try {
+            $sql = $conn->prepare('
+                INSERT INTO tab_servicos
+                    (servico, data_servico, prestador, valor, descricao)
+                VALUES
+                    (:servico, :data_servico, :prestador, :valor, :descricao)
+            ');
+
+            $sql->execute(array(
+                ':servico' => $_POST['txtServico'],
+                ':data_servico' => $_POST['txtData'],
+                ':prestador' => $_POST['txtPrestador'],
+                ':valor' => $_POST['txtValor'],
+                ':descricao' => $_POST['txtDescricao']
+            ));
+
+            if ($sql->rowCount() > 0) {
+                // Redirecionar após o processamento bem-sucedido
+                header("Location: formServicos.php?success=1");
+                exit();
+            }
+
+        } catch (PDOException $erro) {
+            $mensagem = $erro->getMessage();
+        }
+    }
+    // Limpar todos os campos digitados
+    elseif (isset($_POST['btoLimpar'])) {
+        $servico = "";
+        $data_servico = "";
+        $prestador = "";
+        $valor = "";
+        $descricao = "";
+    }
+}
+?>
